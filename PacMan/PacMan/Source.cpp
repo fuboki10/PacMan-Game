ad899289e4df0fd484e3d8bbc91299663356173e
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-06-13 21:53:53 +0200

    First Window

---
+++ PacMan/PacMan/Source.cpp
@@ -0,0 +1,20 @@
+#include <SDL.h>
+
+int main(int args, char** argv)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("PacMan Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
+
+	return 0;
+}
