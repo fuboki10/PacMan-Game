24b9cc370ab44116f5fe33c28e0f950d384d84d8
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-06-14 19:23:43 +0200

    Add DrawWithAngle Function

--- PacMan/PacMan/TextureManager.cpp
+++ PacMan/PacMan/TextureManager.cpp
@@ -23,3 +23,8 @@ void TextureManager::Draw(SDL_Texture* text, SDL_Rect src, SDL_Rect dst, SDL_Ren
 	SDL_RenderCopy(renderer, text, &src, &dst);
 
 }
+
+void TextureManager::DrawWithAngle(SDL_Texture* text, SDL_Rect src, SDL_Rect dst, SDL_Renderer* renderer, const double angle, const SDL_Point* center, SDL_RendererFlip flip)
+{
+	SDL_RenderCopyEx(renderer, text, &src, &dst, angle, center, flip);
+}
--- PacMan/PacMan/TextureManager.h
+++ PacMan/PacMan/TextureManager.h
@@ -7,5 +7,6 @@ class TextureManager
 public:
 	static SDL_Texture* LoadTexture(const char* fileName, SDL_Renderer* ren);
 	static void Draw(SDL_Texture* text, SDL_Rect src, SDL_Rect dst, SDL_Renderer* renderer);
+	static void DrawWithAngle(SDL_Texture* text, SDL_Rect src, SDL_Rect dst, SDL_Renderer* renderer, const double angle, const SDL_Point* center, SDL_RendererFlip flip);
 };
 
